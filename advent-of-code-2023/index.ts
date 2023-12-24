246f5f59faecc20446d19e8c45bd5dad0719440d
Nick Harris <nharris614@gmail.com>
2023-12-24 01:35:30 -0500

    Update to use map

--- advent-of-code-2023/index.ts
+++ advent-of-code-2023/index.ts
@@ -9,23 +9,35 @@ interface ScriptAndSolution {
   correctAnswer: number | string;
 }
 
-const dayNumberToSolution: { [key: number | string]: ScriptAndSolution[] } = {
-  1: [
-    {
-      script: getSumOfCalibrationValues,
-      correctAnswer: 54561,
-    },
-    {
-      script: getSumOfCalibrationValuesFromStrings,
-      correctAnswer: 54076,
-    },
-  ],
-  2: [
-    { script: cubeConundrumPt1, correctAnswer: 2505 },
-    { script: cubeConundrumPt2, correctAnswer: 70265 },
-  ],
-  3: [{ script: gearRatiosPt1, correctAnswer: "unknown" }],
-};
+const dayNumberToSolution = new Map<number, ScriptAndSolution[]>(
+  [
+    [
+      1,
+      [
+        {
+          script: getSumOfCalibrationValues,
+          correctAnswer: 54561,
+        },
+        {
+          script: getSumOfCalibrationValuesFromStrings,
+          correctAnswer: 54076,
+        },
+      ],
+    ],
+    [
+      2,
+      [
+        { script: cubeConundrumPt1, correctAnswer: 2505 },
+        { script: cubeConundrumPt2, correctAnswer: 70265 },
+      ],
+    ],
+    [
+      3,
+      [{ script: gearRatiosPt1, correctAnswer: "unknown" }],
+    ],
+  ]
+
+);
 
 const dayNumber = Number(process.argv[2]);
 
@@ -33,17 +45,16 @@ if (dayNumber) {
   // fixme
   // console.log(dayNumberToSolution[dayNumber]?.());
 } else {
-  const allDays = Object.keys(dayNumberToSolution);
-  allDays.forEach((day: string) => {
-    dayNumberToSolution[day].forEach((part, i) => {
+  dayNumberToSolution.forEach((entry, dayNum) => {
+    entry.forEach((part, i) => {
       const computedAnswer = part?.script();
       const isCorrect = computedAnswer === part?.correctAnswer;
       console.log(
-        `day ${day} pt${i + 1}:`,
+        `day ${dayNum} pt${i + 1}:`,
         computedAnswer,
         "correct: ",
         isCorrect
       );
-    });
+    })
   });
 }
